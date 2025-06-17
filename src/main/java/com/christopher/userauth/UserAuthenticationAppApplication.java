0cc979c2f2cd9f0c762bb0a3d9c301282b75f6b2
Chris <chris3270015@gmail.com>
2025-06-17 16:23:52 +0200

    Started Project Securing a Web Application with Spring boot. Added Hello and Login Htmls, Setup Spring Boot as well as the dependencies. Created WebSecurityConfig.java and added Comments for further understanding.

---
+++ src/main/java/com/christopher/userauth/UserAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.christopher.userauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationAppApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(UserAuthenticationAppApplication.class, args);
+    }
+
+}
