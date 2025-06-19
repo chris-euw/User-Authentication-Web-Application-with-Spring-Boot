e9b6d1767058daeffa5a22bd9475e717d8023d23
Chris <chris3270015@gmail.com>
2025-06-19 10:02:02 +0200

    Deleted SecuringWebApplication.java since it was a duplicate and not needed.

--- src/main/java/com/christopher/userauth/UserAuthenticationAppApplication.java
+++ src/main/java/com/christopher/userauth/UserAuthenticationAppApplication.java
@@ -3,10 +3,14 @@ package com.christopher.userauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+///Marks class as Source of Spring beans
+//Automatically configures the App Based on dependencies
+//Automatically finds and wires up other classes(controllers & services)
 @SpringBootApplication
 public class UserAuthenticationAppApplication {
 
     public static void main(String[] args) throws Throwable {
+        //Boots up the entire Spring context, starts Tomcat serve
         SpringApplication.run(UserAuthenticationAppApplication.class, args);
     }
 
